e2644e818809efa3c9a2d6598e4e90cb79ba5a74
bbubbush <bbubbush@gmail.com>
2021-08-12 23:40:45 +0900

    make project for curry img

---
+++ src/main/java/com/bbubbush/curryimg/CurryImgApplication.java
@@ -0,0 +1,13 @@
+package com.bbubbush.curryimg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurryImgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurryImgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bbubbush/curryimg/CurryImgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbubbush.curryimg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurryImgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
