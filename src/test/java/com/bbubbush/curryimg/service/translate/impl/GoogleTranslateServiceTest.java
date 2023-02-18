689388b161cc913fb99e019a239de2ad0c3de0a7
bbubbush <bbubbush@gmail.com>
2023-02-18 18:35:36 +0900

    Fix: 불필요한 테스트 코드 삭제

--- src/test/java/com/bbubbush/curryimg/service/translate/impl/GoogleTranslateServiceTest.java
+++ src/test/java/com/bbubbush/curryimg/service/translate/impl/GoogleTranslateServiceTest.java
@@ -1,31 +1,18 @@
 package com.bbubbush.curryimg.service.translate.impl;
 
 import com.bbubbush.curryimg.dto.req.TranslateArabicReq;
-import com.bbubbush.curryimg.entity.app.Dictionary;
-import com.bbubbush.curryimg.service.dictionary.DictionaryService;
-import com.google.api.services.translate.model.TranslationsResource;
-import com.google.cloud.translate.Translate;
-import com.google.cloud.translate.TranslateOptions;
-import com.google.cloud.translate.Translation;
-import org.hibernate.sql.ordering.antlr.TranslationContext;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.client.MockRestServiceServer;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.BDDMockito.given;
 
 //@ExtendWith(MockitoExtension.class)
 //@Transactional
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest
 class GoogleTranslateServiceTest {
   @Autowired
   private GoogleTranslateService translateService;
