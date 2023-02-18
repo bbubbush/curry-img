992ccf1329beb7c4dbe7959d6b9d9699a4f13c71
bbubbush <bbubbush@gmail.com>
2023-02-18 18:36:34 +0900

    Feat: 불필요한 코드 삭제

--- src/test/java/com/bbubbush/curryimg/service/translate/impl/GoogleTranslateServiceTest.java
+++ src/test/java/com/bbubbush/curryimg/service/translate/impl/GoogleTranslateServiceTest.java
@@ -8,33 +8,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-//@ExtendWith(MockitoExtension.class)
-//@Transactional
-
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @SpringBootTest
 class GoogleTranslateServiceTest {
   @Autowired
   private GoogleTranslateService translateService;
 
-
-
   @Test
   @DisplayName("구글 번역")
   void translate() {
     // given
-//    TranslationsResource a = new Translate();
-//    Translate translate = TranslateOptions.getDefaultInstance().getService();
-//    given(dictionaryService.findArabicByWord(any())).willReturn(null);
-//    given(TranslateOptions.getDefaultInstance().getService()).willReturn(translate);
-//    given(translate.translate(anyString(), any())).willReturn(new TranslateImpl(any()));
-
-
-
     TranslateArabicReq translateArabicReq = new TranslateArabicReq();
     translateArabicReq.setKeyword("사과");
-    //translateArabicReq.("사과");
-
 
     // when
     final String arabicWord = translateService.translate(translateArabicReq);
